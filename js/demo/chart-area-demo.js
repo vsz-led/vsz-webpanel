fbaf6b058393520496a183bc3e67531bcefc5917
ItIsJQ <julianrijkers@gmail.com>
2023-01-09 11:10:38 +0100

    chart text dark

--- js/demo/chart-area-demo.js
+++ js/demo/chart-area-demo.js
@@ -36,7 +36,7 @@ var myLineChart = new Chart(ctx, {
     datasets: [{
       label: "Earnings",
       lineTension: 0.3,
-      backgroundColor: "rgba(252, 191, 73, 0.05)",
+      backgroundColor: "rgba(26, 29, 32, 0.05)",
       borderColor: "rgba(252, 191, 73, 1)",
       pointRadius: 3,
       pointBackgroundColor: "rgba(252, 191, 73, 1)",

41ee935722a0b6d3b4015afc25ebf31a0b3181e5
ItIsJQ <julianrijkers@gmail.com>
2023-01-12 15:18:17 +0100

    Kleur geel

--- js/demo/chart-area-demo.js
+++ js/demo/chart-area-demo.js
@@ -34,7 +34,7 @@ var myLineChart = new Chart(ctx, {
   data: {
     labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
     datasets: [{
-      label: "Earnings",
+      label: " ",
       lineTension: 0.3,
       backgroundColor: "rgba(26, 29, 32, 0.05)",
       borderColor: "rgba(252, 191, 73, 1)",

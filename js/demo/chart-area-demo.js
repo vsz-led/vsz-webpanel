b248a7ce34f0d4b2498376b20d8f18f5d4ab747a
lhridder <lhridder@gmail.com>
2023-01-14 22:08:16 +0100

    add cars per day to graph

--- js/demo/chart-area-demo.js
+++ js/demo/chart-area-demo.js
@@ -27,92 +27,91 @@ function number_format(number, decimals, dec_point, thousands_sep) {
   return s.join(dec);
 }
 
-// Area Chart Example
-var ctx = document.getElementById("myAreaChart");
-var myLineChart = new Chart(ctx, {
-  type: 'line',
-  data: {
-    labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
-    datasets: [{
-      label: " ",
-      lineTension: 0.3,
-      backgroundColor: "rgba(26, 29, 32, 0.05)",
-      borderColor: "rgba(252, 191, 73, 1)",
-      pointRadius: 3,
-      pointBackgroundColor: "rgba(252, 191, 73, 1)",
-      pointBorderColor: "rgba(252, 191, 73, 1)",
-      pointHoverRadius: 3,
-      pointHoverBackgroundColor: "rgba(252, 191, 73, 1)",
-      pointHoverBorderColor: "rgba(78, 115, 223, 1)",
-      pointHitRadius: 10,
-      pointBorderWidth: 2,
-      data: [0, 10000, 5000, 15000, 10000, 20000, 15000, 25000, 20000, 30000, 25000, 40000],
-    }],
-  },
-  options: {
-    maintainAspectRatio: false,
-    layout: {
-      padding: {
-        left: 10,
-        right: 25,
-        top: 25,
-        bottom: 0
-      }
-    },
-    scales: {
-      xAxes: [{
-        time: {
-          unit: 'date'
-        },
-        gridLines: {
-          display: false,
-          drawBorder: false
-        },
-        ticks: {
-          maxTicksLimit: 7
-        }
-      }],
-      yAxes: [{
-        ticks: {
-          maxTicksLimit: 5,
-          padding: 10,
-          // Include a dollar sign in the ticks
-          callback: function(value, index, values) {
-            return '$' + number_format(value);
+fetch('/client/autosweek')
+    .then((response) => {
+      response.json().then(data => {
+        // Area Chart Example
+        var ctx = document.getElementById("myAreaChart");
+        var myLineChart = new Chart(ctx, {
+          type: 'line',
+          data: {
+            labels: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
+            datasets: [{
+              label: " ",
+              lineTension: 0.3,
+              backgroundColor: "rgba(26, 29, 32, 0.05)",
+              borderColor: "rgba(252, 191, 73, 1)",
+              pointRadius: 3,
+              pointBackgroundColor: "rgba(252, 191, 73, 1)",
+              pointBorderColor: "rgba(252, 191, 73, 1)",
+              pointHoverRadius: 3,
+              pointHoverBackgroundColor: "rgba(252, 191, 73, 1)",
+              pointHoverBorderColor: "rgba(78, 115, 223, 1)",
+              pointHitRadius: 10,
+              pointBorderWidth: 2,
+              data: data.reverse(),
+            }],
+          },
+          options: {
+            maintainAspectRatio: false,
+            layout: {
+              padding: {
+                left: 10,
+                right: 25,
+                top: 25,
+                bottom: 0
+              }
+            },
+            scales: {
+              xAxes: [{
+                time: {
+                  unit: 'date'
+                },
+                gridLines: {
+                  display: false,
+                  drawBorder: false
+                },
+                ticks: {
+                  maxTicksLimit: 7
+                }
+              }],
+              yAxes: [{
+                ticks: {
+                  maxTicksLimit: 5,
+                  padding: 10,
+                },
+                gridLines: {
+                  color: "rgb(252, 191, 73)",
+                  zeroLineColor: "rgb(252, 191, 73)",
+                  drawBorder: false,
+                  borderDash: [2],
+                  zeroLineBorderDash: [2]
+                }
+              }],
+            },
+            legend: {
+              display: false
+            },
+            tooltips: {
+              backgroundColor: "rgb(255,255,255)",
+              bodyFontColor: "#212529",
+              titleMarginBottom: 10,
+              titleFontColor: '#212529',
+              titleFontSize: 14,
+              borderColor: '#dddfeb',
+              borderWidth: 1,
+              xPadding: 15,
+              yPadding: 15,
+              displayColors: false,
+              intersect: false,
+              mode: 'index',
+              caretPadding: 10,
+            }
           }
-        },
-        gridLines: {
-          color: "rgb(252, 191, 73)",
-          zeroLineColor: "rgb(252, 191, 73)",
-          drawBorder: false,
-          borderDash: [2],
-          zeroLineBorderDash: [2]
-        }
-      }],
-    },
-    legend: {
-      display: false
-    },
-    tooltips: {
-      backgroundColor: "rgb(255,255,255)",
-      bodyFontColor: "#212529",
-      titleMarginBottom: 10,
-      titleFontColor: '#212529',
-      titleFontSize: 14,
-      borderColor: '#dddfeb',
-      borderWidth: 1,
-      xPadding: 15,
-      yPadding: 15,
-      displayColors: false,
-      intersect: false,
-      mode: 'index',
-      caretPadding: 10,
-      callbacks: {
-        label: function(tooltipItem, chart) {
-          var datasetLabel = chart.datasets[tooltipItem.datasetIndex].label || '';
-          return datasetLabel + ': $' + number_format(tooltipItem.yLabel);
-        }
-      }
-    }
-  }
-});
+        });
+
+      })
+    })
+    .catch((error) => {
+      console.log(error)
+    })
